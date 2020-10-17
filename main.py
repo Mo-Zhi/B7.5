2366694a821eb80d435ff7e3d8af8f6963fa4e83
mo-zhi <causelesscause@gmail.com>
2020-10-17 19:09:53 +0300

    update

--- main.py
+++ main.py
@@ -2,18 +2,40 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 
-class Board:
-  
 class Player:
-    ships
-    shots
-def addShip(rowStart, colStart, rowEnd,colEnd):
-    if rowStart != rowEnd and colStart != colEnd:
-        sg.Popup('Diagonal ships are not allowed')
-     elseif rowStart == rowEnd and colEnd-colStart>2 or
-                 colStart == colEnd and rowEnd-rowStart>2
-         sg.Popup('Ship is too big')
-      
+  
+    class Board:
+        dimensions
+        ships
+        shots
 
+        def __init__(self, dimensions=6):
+            self.dimensions = dimensions
+            shots = [[ 0 for i in range(dimensions)] for i in range(dimensions)]
+
+        def checkCoordinates(size, row, col, direction='row'):
+           if direction == 'row':
+               for i in range(col):
+                   if 
+        
+        def addShip(size, row, col, direction='row')
+            if shots[row][col]:
+
+            else:
+                ship = Ship(size, row, col, direction)
+                ships.extend(ship.coordinates)
+        
+        class Ship:
+            coordinates
+
+            def __init__(self, size, row, col, direction):
+                if direction == 'row':
+                    for i in range(size):
+                        coordinates.append((row, col + i))
+                elif direction == 'col':
+                    for i in range(size):
+                        coordinates.append((row + i, col))
+
+    
 
         
