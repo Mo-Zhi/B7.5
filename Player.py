3bbccfde6a8624aa1453f7b34ee3ac52013bd340
mo-zhi <causelesscause@gmail.com>
2020-10-29 17:38:35 +0300

    update

--- Player.py
+++ Player.py
@@ -1,23 +1,26 @@
 import random
 
 
-class Board:
-    """A Board has size, an array of player's ships and an array of player's"""
+class Player:
+    """A Player contains all information about his side of the game"""
     _boardSize = 0
 
     class Ship:
         """A Ship has coordinates of its cells."""
 
         def checkCoordinates(self, row, col, ships):
-
+            """Checks if a ship can be placed on this cell"""
             if len(ships) > 0:
-                for tempShip in ships:
-                    for coordinates in tempShip.coordinates:
+                for ship in ships:
+                    for coordinates in ship.coordinates:
                         if row == coordinates[0] and col == coordinates[1]:
                             raise Exception(
-                                'Ship cannot be placed on occupied cells.')
+                                'A ship cannot be placed on occupied cells.')
+                        if (row == coordinates[0] and abs(col - coordinates[1]) < 2) or (abs(row-coordinates[0]) < 2 and col == coordinates[1]):
+                            raise Exception('Other ship is too close')
             if row < 0 or row > self.boardSize-1 or col < 0 or col > self.boardSize-1:
-                raise Exception('Ship is not entirely inside of the board.')
+                raise Exception(
+                    'The ship is not entirely inside of the board.')
 
             return True
 
@@ -29,6 +32,7 @@ class Board:
             self._coordinates = []
             tempCoordinates = []
 
+#           Picking random coordinates and the direction if it is enemy's ship
             if direction == '':
                 while True:
                     row = random.randint(0, boardSize-1)
@@ -39,18 +43,19 @@ class Board:
                         continue
                     else:
                         if random.randint(0, 1) == 0:
-                            direction = 'row'
+                            direction = 'Row'
                         else:
-                            direction = 'column'
+                            direction = 'Column'
                         break
-            if direction == 'row':
+            if direction == 'Row':
                 for i in range(size):
                     if self.checkCoordinates(row, col+i, ships):
                         tempCoordinates.append((row, col + i))
-            elif direction == 'column':
+            elif direction == 'Column':
                 for i in range(size):
                     if self.checkCoordinates(row + i, col, ships):
                         tempCoordinates.append((row + i, col))
+
             self.coordinates = tempCoordinates
 
         @property
@@ -69,45 +74,40 @@ class Board:
         def direction(self):
             return self._direction
 
-    def addShip(self, size=1, row=-1, col=-1,
-                direction='', boardSize=6):
-        """Adds a new Ship to the array of Ships and returns its coordinates as an array of tuples"""
+    def addShip(self, size=1, row=-1, col=-1, boardSize=6):
+        """Adds a new ship to the array of player's ships and returns its coordinates as an array of tuples"""
 
-        ship = self.Ship(size, row, col, direction,
+        ship = self.Ship(size, row, col, self.direction,
                          boardSize, self.ships)
         self.ships.append(ship)
         return ship.coordinates
 
+    def changeDirection(self):
+        '''Changes direction from \'Row\' to \'Column\''''
+        if self._direction == 'Row':
+            self._direction = 'Column'
+        else:
+            self._direction = 'Row'
+
     def __init__(self, isEnemy=True, boardSize=6):
         self._boardSize = boardSize
         self._occupiedCells = 11
+        self._isEnemy = isEnemy
+        self._direction = 'Row'
         self._ships = []
-        self._shots = [
-            [0 for i in range(boardSize)]
-            for i in range(boardSize)
-        ]
+        self._shots = []
 
         if isEnemy:
-            for _ in range(4):
-                while True:
-                    try:
-                        self.addShip()
-                    except:
-                        continue
-                    else:
-                        break
-            for _ in range(2):
-                while True:
-                    try:
-                        self.addShip(2)
-                    except:
-                        continue
-                    else:
-                        break
+            self._direction = ''
             while True:
                 try:
+                    for _ in range(4):
+                        self.addShip()
+                    for _ in range(2):
+                        self.addShip(2)
                     self.addShip(3)
                 except:
+                    self.ships.clear()
                     continue
                 else:
                     break
@@ -115,6 +115,14 @@ class Board:
     @property
     def ships(self):
         return self._ships
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @property
+    def isEnemy(self):
+        return self._isEnemy
 
     @property
     def shots(self):
--- main.py
+++ main.py
@@ -1,75 +1,128 @@
 from PySimpleGUI import PySimpleGUI as sg
-from Board import Board
+from Player import Player
+import random
 
 sg.theme('DarkAmber')
-
 boardSize = 6
-player = Board(False)
-enemy = Board()
-leftText = [
-    [sg.Text('TEXT', key='-TEXT-SHIPS-')]
-]
+player = Player(False)
+enemy = Player()
+
+buttonDirection = [sg.Text('Direction:', key='-TEXT-DIRECTION-'),
+                   sg.Button('Row', key='-DIRECTION-')]
 leftBoard = [
     [sg.Button(size=(1, 1), key=f'-SHIP{i}{j}-')
      for j in range(boardSize)] for i in range(boardSize)
 ]
-leftColumn = leftText + leftBoard
-rightText = [
-    [sg.Text('TEXT', key='-TEXT-SHOTS-')]
-]
 rightBoard = [
-    [sg.Button(size=(1, 1), key=f'-SHOT{i}{j}-')
+    [sg.Button(size=(1, 1), key=f'-SHOT{i}{j}-', disabled=True)
      for j in range(boardSize)] for i in range(boardSize)
 ]
-rightColumn = rightText + rightBoard
 layout = [
-    [sg.Column(leftColumn),
+    buttonDirection,
+    [sg.Column(leftBoard),
      sg.VerticalSeparator(),
-     sg.Column(rightColumn)]
+     sg.Column(rightBoard)]
 ]
 window = sg.Window('Sea Battle', layout)
 
 
 def updateButton(event, buttonColor=('black', 'black'), isDisabled=True):
-
+    """Changes state of the button: color and/or is it disabled"""
     if buttonColor == None:
         window[event].update(disabled=isDisabled)
     else:
         window[event].update(disabled=isDisabled, button_color=buttonColor)
 
 
+def makeAShot(oneBeingShot, event=None):
+    """Handles effects of shots"""
+#   If the event is 'None' then the one shooting is the computer,
+#       so the coordinates of the shot will be random. If they are duplicates,
+#       they are picked again
+    if event == None:
+        while True:
+            wasShotBefore = False
+            row, col = random.randint(
+                0, boardSize - 1), random.randint(0, boardSize - 1)
+            if len(oneBeingShot.shots) > 0:
+                for shot in oneBeingShot.shots:
+                    if row == shot[0] and col == shot[1]:
+                        wasShotBefore = True
+                        break
+            if not wasShotBefore:
+                break
+        oneBeingShot.shots.append((row, col))
+        event = '-SHIP'+str(row)+str(col)+'-'
+    else:
+        row, col = int(event[5]), int(event[6])
+
+#   If the cell is occupied, change it to grey
+#       If not, change it to white
+    cellIsOccupied = False
+    for ship in oneBeingShot.ships:
+        if cellIsOccupied:
+            break
+        for coordinates in ship.coordinates:
+            if row == coordinates[0] and col == coordinates[1]:
+                cellIsOccupied = True
+                oneBeingShot.occupiedCells = -1
+                break
+    if cellIsOccupied:
+        updateButton(event, ('grey', 'grey'), True)
+    else:
+        updateButton(event, ('white', 'white'), True)
+
+#   If some player has no ships left, game is over
+#       and all buttons are disabled
+    if oneBeingShot.occupiedCells == 0:
+        if oneBeingShot.isEnemy:
+            sg.popup('Enemy has no ships left, you win')
+        else:
+            sg.popup('You have no ships left, you lost')
+
+        for i in range(boardSize):
+            for j in range(boardSize):
+                updateButton(f'-SHOT{i}{j}-', None, True)
+
+
+# event handler
 while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED or event == 'Cancel':
         break
 
+#   If button on top is pressed
+    elif event == '-DIRECTION-':
+        player.changeDirection()
+        window[event].update(player.direction)
+
+#   If a button on the left board is pressed
     elif event[1:5] == 'SHIP':
         row = int(event[5])
         col = int(event[6])
         shipsAmount = len(player.ships)
 
+#       Adding ships based on how many ships you already have
         if shipsAmount < 4:
             try:
-                player.addShip(1, row, col, 'row')
+                player.addShip(1, row, col)
             except Exception as e:
                 sg.popup(str(e))
             else:
                 updateButton(event)
-
         elif shipsAmount < 6:
             try:
-                coordinates = player.addShip(2, row, col, 'row')
+                coordinates = player.addShip(2, row, col)
             except Exception as e:
                 sg.popup(str(e))
             else:
                 for i in range(len(coordinates)):
                     updateButton(event[:5] + str(coordinates[i][0]) + str(
                         coordinates[i][1]) + '-')
-
         elif shipsAmount == 6:
             try:
-                coordinates = player.addShip(3, row, col, 'row')
+                coordinates = player.addShip(3, row, col)
             except Exception as e:
                 sg.popup(str(e))
             else:
@@ -81,25 +134,10 @@ while True:
                         updateButton(f'-SHIP{i}{j}-', None, True)
                         updateButton(f'-SHOT{i}{j}-', None, False)
 
+    # If a button on the right board is pressed
     elif event[1:5] == 'SHOT':
-        row, col = int(event[5]), int(event[6])
-        cellIsOccupied = False
-        for tempShip in enemy.ships:
-            if cellIsOccupied:
-                break
-            for coordinates in tempShip.coordinates:
-                if row == coordinates[0] and col == coordinates[1]:
-                    cellIsOccupied = True
-                    enemy.occupiedCells = -1
-                    break
-
-        if cellIsOccupied:
-            updateButton(event, ('grey', 'grey'), True)
-        else:
-            updateButton(event, ('white', 'white'), True)
-
-        if enemy.occupiedCells == 0:
-            sg.popup('Enemy has no ships left, you win')
+        makeAShot(enemy, event)
+        makeAShot(player)
 
 
 window.close()
